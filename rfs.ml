c78ffb879550db04a7e5a95023203406dcb2f146
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2021-02-04 16:40:53 +0100

    number of features to choose as an option

--- rfs.ml
+++ rfs.ml
@@ -9,6 +9,8 @@ let train_x = ref ""
 let train_y = ref ""
 let test_x = ref ""
 let n = ref 10
+let m = ref 0
+let max_depth = ref 10
 let split_rule = ref "gini"
 let data_type = ref "sparse"
 
@@ -23,6 +25,8 @@ let speclist =
         ("-train_y", Arg.Set_string train_y, "Training data, labels.");
         ("-test_x", Arg.Set_string test_x, "Testing data, features.");
         ("-n", Arg.Set_int n, "Number of trees.");
+        ("-m", Arg.Set_int m, "Number of features to choose from.");
+        ("-max_depth", Arg.Set_int max_depth, "Maximal depth of trees.");
         ("-split_rule", Arg.Symbol (["gini"; "random"], set_rule),
             "Split rule for building trees.");
     ]
@@ -35,10 +39,11 @@ let () = Arg.parse
 
 let train_examples = load !train_x ~labels:!train_y
 let test_examples = load !test_x
-let tree = match !split_rule with
-    | "random" -> Tree.tree random_rule
-    | "gini" -> Tree.tree gini_rule
+let split_rule = match !split_rule with
+    | "random" -> random_rule
+    | "gini" -> gini_rule ~m:!m
     | _ -> failwith "Bad random rule specification."
+let tree = Tree.tree split_rule ~max_depth:!max_depth
 let forest = forest tree !n train_examples
 let preds = classify forest test_examples
 let () = List.iter print_label preds
