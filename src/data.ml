2117b905142ff9987974333ebf027b938afcd50a
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2021-03-19 01:38:24 +0100

    sqrt examples + sqrt labels

--- src/data.ml
+++ src/data.ml
@@ -152,10 +152,11 @@ exception Empty_list
 (* m -- numbers of features to choose from *)
 let gini_rule examples =
     let n = length examples in (* more examples = more features to consider *)
-(*     let m = n |> float_of_int |> sqrt |> int_of_float  in *)
+    let m1 = n |> float_of_int |> sqrt |> int_of_float  in
 (*     let m = 10 in *)
-    let m = List.length (Utils.uniq (labels examples))
+    let m2 = List.length (Utils.uniq (labels examples))
             |> float_of_int |> sqrt |> int_of_float in
+    let m = m1 + m2 in
     let random_feas = random_features examples m in
     let rec loop features impurs =
         match features with
