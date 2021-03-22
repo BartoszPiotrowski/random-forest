97db8093590f66043219a55741ea6b7abded844f
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2021-03-22 17:10:32 +0100

    all features

--- src/data.ml
+++ src/data.ml
@@ -181,7 +181,8 @@ let gini_rule examples =
     let m = (m1 + m2) |> float_of_int |> sqrt |> int_of_float in
 (*     let random_feas = random_features examples m in *)
     let splitting_feas = splitting_features examples in
-    let random_feas = [Utils.choose_random splitting_feas] in
+(*     let random_feas = [Utils.choose_random splitting_feas] in *)
+    let random_feas = splitting_feas in
     let rec loop features impurs =
         match features with
         | [] -> List.rev impurs
