76cb62bd80aff7d610c7709922f8e5b15ae005bc
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2021-03-25 00:03:30 +0100

    better data loading

--- src/utils.ml
+++ src/utils.ml
@@ -71,14 +71,8 @@ let time f x =
 
 let load_features file =
     let lines = read_lines file in
-    let () = Printf.printf "AAA\n" in
     let split = Str.split_delim (Str.regexp " ") in
-    let rec loop split_lines = function
-        | [] -> List.rev split_lines
-        | h :: t ->
-            let features_list = List.map int_of_string (split h) in
-            features_list :: (loop split_lines t) in
-    loop [] lines
+    List.map (fun l -> List.map int_of_string (split l)) lines
 
 let load_labels file =
     List.map int_of_string (read_lines file)
