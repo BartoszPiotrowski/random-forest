68bbbdd7e56db444e8d901e890d2bff826d849b2
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2021-03-26 03:22:48 +0100

    fix

--- src/forest_online.ml
+++ src/forest_online.ml
@@ -3,11 +3,11 @@ module Make = functor (Data : Tree_online.DATA) -> struct
 
     let empty = []
 
-    let add ?(min_impur=0.5) ?(n_trees=1000) ?(remove_old=false)
+    let add ?(min_impur=0.5) ?(n_trees=100) ?(remove_old=false)
         forest example =
         let n = List.length forest in
         let add_tree = (n = 0) || ((Random.int n = 0) && n < n_trees) in
-        let del_tree = remove_old && n > n_trees in
+        let del_tree = remove_old && n >= n_trees in
         let forest = if del_tree then Utils.remove_last forest else forest in
         let updated_trees =
             List.map (fun tree -> Tree.add ~min_impur tree example) forest in
