78f04791a4a2bda2d49625feb0e65e00874692db
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2021-11-17 12:55:47 +0100

    old test fixed

--- test/tree_online_sparse_test.ml
+++ test/tree_online_sparse_test.ml
@@ -1,16 +1,16 @@
 open Tree_online.Make(Data)
-open Data
 open Printf
+open Utils
 
-let examples_train =
-    load "test/data/field_theory_train.features"
-    ~labels:"test/data/field_theory_train.labels"
-let examples_test =
-    load "test/data/field_theory_test.features"
-    ~labels:"test/data/field_theory_test.labels"
-let labels_test = labels examples_test ;;
+let () = printf "Loading training data... %!"
+let features = load_features "test/data/field_theory_train.features"
+let labels = load_labels "test/data/field_theory_train.labels"
+let () = printf "Done.\n%!"
+let () = printf "Number of unique labels: %n\n" (List.length (Utils.uniq labels))
+let () = printf "Number of examples: %n\n" (List.length features)
+let examples = List.combine features labels
+let examples = List.map Data.labeled examples
+let tree1 = tree examples
+let preds1 = List.map (fun e -> classify e tree1) examples
+let () = print_float (accuracy labels preds1)
 
-printf "tree1\n";;
-let tree1 = tree examples_train ;;
-let preds1 = classify examples_test tree1;;
-print_float (Utils.accuracy labels_test preds1);;
