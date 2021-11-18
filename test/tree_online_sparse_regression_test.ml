6d4da19d15396c6074731df70313f678cadfc3c7
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2021-11-18 11:46:40 +0100

    regression tree working fine

---
+++ test/tree_online_sparse_regression_test.ml
@@ -0,0 +1,18 @@
+open Tree_online_regression.Make(Data)
+open Printf
+open Utils
+
+let () = printf "Loading training data... %!"
+let features = load_features "test/data/synthetic_binary_train.features"
+let labels = load_labels "test/data/synthetic_binary_train.labels"
+let () = printf "Done.\n%!"
+let () = printf "Number of unique labels: %n\n" (List.length (Utils.uniq labels))
+let () = printf "Number of examples: %n\n" (List.length features)
+let examples = List.combine features labels
+let examples = List.map Data.labeled examples
+let tree = tree examples
+let preds = List.map (fun e -> classify e tree) examples
+(* let () = List.iter (fun x -> printf "%f\n" x) preds *)
+let () = print_float (rmse labels preds)
+
+
